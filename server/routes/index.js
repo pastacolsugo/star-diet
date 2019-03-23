9af3eebe8a9f9d69a10cec3f2d099780e8327103
Ugo Baroncini <ugobaroncini@gmail.com>
2019-03-23 13:04:44 +0100

    Updated weight display and request headers

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,26 +4,32 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.sendFile(path.join(__dirname, '../../index.html'));
 });
 
 router.get('/style.css', function(req, res, next){
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.sendFile(path.join(__dirname, '../../style.css'));
 });
 
 router.get('/butter', function(req, res, next){
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.sendFile(path.join(__dirname, '../../butter.html'));
 });
 
 router.get('/star-diet', function(req, res, next){
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.sendFile(path.join(__dirname, '../../butter.html'));
 });
 
 router.get('/Roboto.ttf', function(req, res, next){
+	res.setHeader('Access-Control-Allow-Origin', '*');	
 	res.sendFile(path.join(__dirname, '../font/Roboto/Roboto-regular.ttf'));
 })
 
 router.get('/butter.css', function(req, res, next){
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.sendFile(path.join(__dirname, '../../butter.css'));
 });
 
